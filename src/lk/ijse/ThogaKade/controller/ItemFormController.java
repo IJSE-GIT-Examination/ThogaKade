f438bbe781c4bdae0161ef356620d17027f1935b
Sandaru293 <skithshan93@gmail.com>
2021-10-09 11:28:07 +0530

    add AppInitializer

--- src/lk/ijse/ThogaKade/controller/ItemFormController.java
+++ src/lk/ijse/ThogaKade/controller/ItemFormController.java
@@ -4,5 +4,5 @@ package lk.ijse.ThogaKade.controller;
  * @author Sandaru Kithshan <skithshan93@gmail.com> (github.com/Sandaru293)
  * @since 10/9/2021
  */
-public class ItemController {
+public class ItemFormController {
 }
