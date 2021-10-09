3fa9637287fd150c06cc6dd2b0b98578d2ba5e22
Sandaru293 <skithshan93@gmail.com>
2021-10-09 10:50:22 +0530

    create ItemForm.fxml

--- src/lk/ijse/ThogaKade/controller/ItemFormController.java
+++ src/lk/ijse/ThogaKade/controller/ItemFormController.java
@@ -1,8 +1,18 @@
 package lk.ijse.ThogaKade.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 /**
  * @author Sandaru Kithshan <skithshan93@gmail.com> (github.com/Sandaru293)
  * @since 10/9/2021
  */
 public class ItemFormController {
+    public TextField txtCode;
+    public TextField txtdescription;
+    public TextField txtunitPrice;
+    public TextField txtqtyOnHand;
+
+    public void btnSaveItemOnAction(ActionEvent actionEvent) {
+    }
 }
