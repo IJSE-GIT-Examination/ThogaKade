fc8b63ccf8734941671ea86cd94fd520c937caf9
Sandaru293 <skithshan93@gmail.com>
2021-10-09 10:38:57 +0530

    dbConnection class finished

--- src/lk/ijse/ThogaKade/db/DBConnection.java
+++ src/lk/ijse/ThogaKade/db/DBConnection.java
@@ -10,12 +10,14 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static DBConnection dbConnection;
+    //
     private Connection connection;
 
     private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ThogaKade","root","mysql");
     }
+
 
     public Connection getConnection(){
         return connection;
