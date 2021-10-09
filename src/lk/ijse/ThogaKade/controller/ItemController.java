022a4fe94c042502b7644ee14fc7cc9c01abf873
Sandaru293 <skithshan93@gmail.com>
2021-10-09 11:16:59 +0530

    create CrudUtil class

---
+++ src/lk/ijse/ThogaKade/controller/ItemController.java
@@ -0,0 +1,16 @@
+package lk.ijse.ThogaKade.controller;
+
+import lk.ijse.ThogaKade.model.Item;
+import lk.ijse.ThogaKade.utils.CrudUtil;
+
+import java.sql.SQLException;
+
+/**
+ * @author Sandaru Kithshan <skithshan93@gmail.com> (github.com/Sandaru293)
+ * @since 10/9/2021
+ */
+public class ItemController {
+    public boolean addItem(Item i) throws ClassNotFoundException, SQLException {
+        return CrudUtil.execute("INSERT into Item VALUES (?,?,?,?)",i.getCode(),i.getDescription(),i.getUnitPrice(),i.getQtyOnHand());
+    }
+}
