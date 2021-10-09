9addca8038d5964f14816523752031fb7dbce52e
Sandaru293 <skithshan93@gmail.com>
2021-10-09 10:37:15 +0530

    customerForm finished

--- src/lk/ijse/ThogaKade/controller/CustomerFormController.java
+++ src/lk/ijse/ThogaKade/controller/CustomerFormController.java
@@ -1,8 +1,39 @@
 package lk.ijse.ThogaKade.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import lk.ijse.ThogaKade.model.Customer;
+
+import java.sql.SQLException;
+
 /**
  * @author Sandaru Kithshan <skithshan93@gmail.com> (github.com/Sandaru293)
  * @since 10/9/2021
  */
 public class CustomerFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void btnSaveCustomerOnAction(ActionEvent actionEvent) {
+        Customer c1 = new Customer(
+                txtId.getText(),
+                txtName.getText(),
+                txtAddress.getText(),
+                Double.parseDouble(txtAddress.getText())
+        );
+
+        try {
+            if (new CustomerController().addCustomer(c1)){
+                new Alert(Alert.AlertType.CONFIRMATION, "Saved").show();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
